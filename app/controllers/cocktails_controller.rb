361d8ebc11d4b12f6c7be3473854c0cd80210c4d
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-15 00:04:29 +0200

    redirect to

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -17,7 +17,7 @@ before_action :set_cocktail, only: [:show]
   def create
     @cocktail =Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
